77b708af97a15ef03ef4e71bcf8b099ac979ab31
Maria Aurelice <144960412+Aurelice12@users.noreply.github.com>
2024-10-28 16:35:09 -0300

    Create fundamentos05.js

---
+++ fundamentos/fundamentos05.js
@@ -0,0 +1,15 @@
+let num1 = 50
+let num2 = 70
+let num3 = 90
+
+if (num1 & num2 & num3 < 50){
+  console.log('Erro!')
+}
+
+if (num1 & num2 & num3 > 99){
+  console.log('Erro!')
+}
+
+else{
+  console.log('Sucesso!')
+}
