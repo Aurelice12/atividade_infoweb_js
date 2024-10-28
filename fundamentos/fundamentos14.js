c93c306ae877b36b8653171c937e67f5ee9bb712
Maria Aurelice <144960412+Aurelice12@users.noreply.github.com>
2024-10-28 17:38:01 -0300

    Create fundamentos14.js

---
+++ fundamentos/fundamentos14.js
@@ -0,0 +1,12 @@
+let frase = 'Isso é definitivamente uma frase'
+let palavra = 'definitivamente'
+
+let presente = frase.includes(palavra)
+
+if(presente){
+  console.log('A palavra está na frase')
+}
+
+else{
+  console.log('A palavra não está na frase')
+}
