38505eceb500e22ac7a8f2978b81a2582a7a9e97
Maria Aurelice <144960412+Aurelice12@users.noreply.github.com>
2024-10-28 16:52:07 -0300

    Create fundamentos07.js

---
+++ fundamentos/fundamentos07.js
@@ -0,0 +1,13 @@
+let frase = 'Isso é definitivamente uma frase'
+let tamanho = frase.length
+
+let maiusculo = frase.toUpperCase()
+let minusculo = frase.toLowerCase()
+
+if (tamanho < 5){
+  console.log(maiusculo)
+}
+
+else{
+  console.log(minusculo)
+}
