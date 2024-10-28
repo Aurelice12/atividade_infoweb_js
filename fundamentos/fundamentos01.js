2fc1cfcf9297bbfb3c7432ac2d3bd152776df7c9
Maria Aurelice <144960412+Aurelice12@users.noreply.github.com>
2024-10-28 15:54:30 -0300

    Create fundamentos01.js

---
+++ fundamentos/fundamentos01.js
@@ -0,0 +1,7 @@
+let dia = 'segunda-feira'
+let horas = 15
+let minutos = 45
+let segundos = 21
+
+console.log('Hoje é dia:', dia)
+console.log('A hora atual é: ', horas, ':', minutos, ':', segundos)
